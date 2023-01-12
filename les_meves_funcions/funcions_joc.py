492f6013c597b965a4da6a5226511f6a47c239ed
Ifurio <ifuriomartin.cf@iesesteveterradas.cat>
2023-01-12 19:01:16 +0100

    pillo version 12/01/2023 19:01

--- les_meves_funcions/funcions_joc.py
+++ les_meves_funcions/funcions_joc.py
@@ -1,4 +1,13 @@
-from les_meves_funcions.datos import *
+# Funcion de comprovaciones
+from datos import *
+def check_settings():
+    if contextGame["players"] < 2:
+        return print("Select 2 players or more to start the game!")
 
-def prueba():
-    print(contextGame["round"])
+    elif contextGame["deck"] == "":
+        return print("Select a card deck to start the game!")
+
+    elif contextGame["round"] < 5:
+        return print("Select 5 round or more!")
+
+    else:
